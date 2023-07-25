02236f668068d76c5ac177f074e32d1512680f07
Wahyu Aprilliandhika <wahyuandhika910@gmail.com>
2023-07-26 00:00:21 +0700

    update model and controller in backend

--- backend/routes/workout.js
+++ backend/routes/workout.js
@@ -1,20 +1,17 @@
 const express = require("express");
+const {
+  createWorkout,
+  getWorkouts,
+  getWorkout,
+  deleteWorkout,
+  updateWorkout,
+} = require("../controllers/workoutController");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.json({ mssg: "get all workouts" });
-});
-router.get("/:id", (req, res) => {
-  res.json({ mssg: "get a single workouts" });
-});
-router.post("/", (req, res) => {
-  res.json({ mssg: "Post a new workout" });
-});
-router.delete("/:id", (req, res) => {
-  res.json({ mssg: "delete a workouts" });
-});
-router.patch("/:id", (req, res) => {
-  res.json({ mssg: "Update a workout" });
-});
+router.get("/", getWorkouts);
+router.get("/:id", getWorkout);
+router.post("/", createWorkout);
+router.delete("/:id", deleteWorkout);
+router.patch("/:id", updateWorkout);
 module.exports = router;
