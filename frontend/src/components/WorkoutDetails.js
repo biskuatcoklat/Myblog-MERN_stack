974c592b2a88cb1497a560a488e06632c85267e5
Wahyu Aprilliandhika <wahyuandhika910@gmail.com>
2023-07-27 11:23:26 +0700

    finishing

---
+++ frontend/src/components/WorkoutDetails.js
@@ -0,0 +1,37 @@
+import { useWorkoutsContext } from "../hooks/useWokoutsContext";
+import formatDistanceToNow from "date-fns/formatDistanceToNow";
+
+const WorkoutDetails = ({ workout }) => {
+  const { dispatch } = useWorkoutsContext();
+  const handleClick = async () => {
+    const response = await fetch("/api/workout/" + workout._id, {
+      method: "DELETE",
+    });
+    const json = await response.json();
+
+    if (response.ok) {
+      dispatch({ type: "DELETE_WORKOUT", payload: json });
+    }
+  };
+
+  return (
+    <div className="workout-details">
+      <h4>{workout.title}</h4>
+      <p>
+        <strong>Load (Kg): </strong>
+        {workout.load}
+      </p>
+      <p>
+        <strong>Reps : </strong>
+        {workout.reps}
+      </p>
+      <p>
+        {formatDistanceToNow(new Date(workout.createdAt), { addSuffix: true })}
+      </p>
+      <span className="material-symbols-outlined" onClick={handleClick}>
+        Delete
+      </span>
+    </div>
+  );
+};
+export default WorkoutDetails;
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,34 @@
+import { useEffect } from "react";
+import { useWorkoutsContext } from "../hooks/useWokoutsContext";
+
+import WorkoutDetails from "../components/WorkoutDetails";
+import WorkoutForm from "../components/WorkoutForm";
+
 const Home = () => {
+  const { workouts, dispatch } = useWorkoutsContext();
+
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch("/api/workout");
+      const json = await response.json();
+
+      if (response.ok) {
+        dispatch({ type: "SET_WORKOUTS", payload: json });
+      }
+    };
+
+    fetchWorkouts();
+  }, [dispatch]);
+
   return (
     <div className="home">
-      <h2>Home</h2>
+      <div className="workouts">
+        {workouts &&
+          workouts.map((workout) => (
+            <WorkoutDetails key={workouts._id} workout={workout} />
+          ))}
+      </div>
+      <WorkoutForm />
     </div>
   );
 };
